e98fa5fa4ace45aa981bca630908da855ced40ce
Hadrien Kohl <hadrien.kohl@teletopia.com>
2015-05-12 18:17:18 +0200

    Clean the karma.conf.js configuration file

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,8 @@
 module.exports = function (config) {
     config.set({
-        
-        browsers: ['PhantomJS'],
 
-        // Karma config using:
-        //  configFile: 'karma.conf.js',
-        // or direct options
+        browsers: ['PhantomJS'],
         frameworks: ['mocha', 'chai', 'sinon-chai'],
-        /*plugins: [
-            'karma-mocha',
-            'karma-chai',
-            'karma-sinon-chai'
-            //'karma-sinon',
-            //'karma-browserify',
-            //'karma-phantomjs-launcher'
-        ],*/
         files: [
             'plugin.js',
             'test.coffee'
